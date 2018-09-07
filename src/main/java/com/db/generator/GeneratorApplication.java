e154ad919020947cab4f8cfff23758667db703b7
Anastasiia Turenko <aaturenko@gmail.com>
2018-09-07 17:36:18 +0300

    add eureka

--- src/main/java/com/db/generator/GeneratorApplication.java
+++ src/main/java/com/db/generator/GeneratorApplication.java
@@ -2,14 +2,14 @@ package com.db.generator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableRetry
-//@EnableEurekaServer
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class GeneratorApplication {
     public static void main(String[] args) {
         SpringApplication.run(GeneratorApplication.class, args);
